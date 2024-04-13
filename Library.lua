08bad6020de6ecf648d4ec5fc0d0ea888f761e76
MercyfulSmoked <86445121+MercyfulSmoked@users.noreply.github.com>
2024-04-13 15:50:04 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2450,8 +2450,8 @@ function Library:CreateWindow(...)
     });
 
     local WindowLabel = Library:CreateLabel({
-        Position = UDim2.new(0, 7, 0, 25);
-        Size = UDim2.new(0, 0, 0, 0);
+        Position = UDim2.new(0, 40, 0, 0);
+        Size = UDim2.new(0, 0, 0, 25);
         Text = Config.Title or '';
         TextXAlignment = Enum.TextXAlignment.Center;
         ZIndex = 1;
