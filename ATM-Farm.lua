013e5bd452893fc0511f134478b7708c38850373
MercyfulSmoked <86445121+MercyfulSmoked@users.noreply.github.com>
2024-05-28 20:12:49 -0400

    Update ATM-Farm.lua

--- ATM-Farm.lua
+++ ATM-Farm.lua
@@ -1,24 +1,17 @@
-
-local Settings = {
-    Graphics = {
-        LowGFX = true;
-    },
-    Combat = {
-        Equip = 'Knife';  -- Tool to equip for combat
-    },
-    Cash = {
-        Collecting = false; -- Don't Touch
-    }
-}
-
 if Settings.Graphics.LowGFX then
     loadstring(game:HttpGet('https://pastebin.com/raw/eHEfAR8z', true))()
-    game:GetService("RunService"):Set3dRenderingEnabled(true)
+elseif Settings.Graphics.NoRender then
+    game:GetService("RunService"):Set3dRenderingEnabled(false)
 end;
 
 task.spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/MercyfulSmoked/localhub/main/Dahood-Antiban'))()
 end)
+
+task.spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MercyfulSmoked/localhub/main/MoneyCalc.lua'))()
+end)
+
 
 local VirtualUser = game:GetService('VirtualUser')
 game:GetService('Players').LocalPlayer.Idled:connect(function()
@@ -27,8 +20,11 @@ game:GetService('Players').LocalPlayer.Idled:connect(function()
 end)
 
 local LocalPlayer = game.Players.LocalPlayer
-local VIM = game:GetService('VirtualInputManager')
 local Camera = workspace.CurrentCamera
+
+local VIM = game:GetService('VirtualInputManager')
+
+local mag = 14
 
 local random = math.random(0, 1000000)
 if not workspace.Players:FindFirstChild('Safespot') then
@@ -59,7 +55,7 @@ end
 function EquipCombatTool()
     if LocalPlayer and LocalPlayer.Backpack and Alive() then
         local combatTool = LocalPlayer.Backpack:FindFirstChild(Settings.Combat.Equip)
-        if combatTool and combatTool:IsA('Tool') then
+        if combatTool and combatTool:IsA('Tool') and not LocalPlayer.Character:FindFirstChild(Settings.Combat.Equip) then
             local humanoid = LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
             if humanoid and humanoid.Health > 0 then
                 humanoid:EquipTool(combatTool)
@@ -81,7 +77,7 @@ function checkcash()
     local closestPart = nil
     for _, v in ipairs(workspace.Cashiers:GetChildren()) do
         for _, part in ipairs(workspace.Ignored.Drop:GetChildren()) do
-            if part.Name == 'MoneyDrop' and LocalPlayer:DistanceFromCharacter(part.Position) < 17 then
+            if part.Name == 'MoneyDrop' and (LocalPlayer:DistanceFromCharacter(part.Position) < mag and (part.Position - v.Open.Position).Magnitude < mag) then
                 if part.Parent == nil then
                     return nil
                 end
@@ -104,23 +100,28 @@ function FindChild(parent, searchText)
     return nil
 end
 
-local LastCashier = nil
+function CheckWeapon()
+    if Settings.Combat.Equip == 'Knife' and Alive() then
+        repeat task.wait()
+            local Knife = FindChild(workspace.Ignored.Shop, 'Knife')
+            if Knife:FindFirstChild('Head') and Knife:FindFirstChildOfClass('ClickDetector') then
+                fireclickdetector(Knife:FindFirstChildOfClass('ClickDetector'))
+                task.wait(0.05)
+                LocalPlayer.Character:MoveTo(Knife.Head.Position + Vector3.new(0,-7,0))
+            end;
+        until LocalPlayer.Backpack:FindFirstChild('[Knife]') or LocalPlayer.Character:FindFirstChild('[Knife]')
+        Settings.Combat.Equip = '[Knife]'
+    end;
+end;
+
+local LastCashier = nil;
+
 function Atms()
     task.spawn(function()
 
-        if Settings.Combat.Equip == 'Knife' and Alive() then
-            repeat task.wait()
-                local Knife = FindChild(workspace.Ignored.Shop, 'Knife')
-                if Knife:FindFirstChild('Head') and Knife:FindFirstChildOfClass('ClickDetector') then
-                    fireclickdetector(Knife:FindFirstChildOfClass('ClickDetector'))
-                    task.wait(0.05)
-                    LocalPlayer.Character:MoveTo(Knife.Head.Position + Vector3.new(0,-7,0))
-                end;
-            until LocalPlayer.Backpack:FindFirstChild('[Knife]') or LocalPlayer.Character:FindFirstChild('[Knife]')
-            Settings.Combat.Equip = '[Knife]'
-        end;
-
+        CheckWeapon()
         LocalPlayer.DevCameraOcclusionMode = 'Invisicam'
+
         while task.wait() do
             local CashierLocked = false
             for _, Cashier in ipairs(workspace.Cashiers:GetChildren()) do
@@ -130,72 +131,83 @@ function Atms()
             end
 
             if not CashierLocked then
-                Settings.Cash.Collecting = false
                 safespot()
             end
 
-            if checkcash() and Alive() then
-                UnequipTools()
-                repeat
-                    local GotCash = false;
-                    for _, v in pairs(workspace.Ignored.Drop:GetChildren()) do
-                        if (v.Name == 'MoneyDrop') and (v:FindFirstChild('ClickDetector')) and LocalPlayer:DistanceFromCharacter(v.Position) < 17 then
-                            Settings.Cash.Collecting = true
-                            GotCash = true
-                            LocalPlayer.CameraMinZoomDistance = 10
-                            LocalPlayer.CameraMaxZoomDistance = 10
-                            if LastCashier then
-                                Camera.CameraSubject = LastCashier
-                                LocalPlayer.Character:MoveTo(LastCashier.Position + Vector3.new(0, 3, 0))
-                            end
-                            fireclickdetector(v.ClickDetector)
-                            task.wait(0.1)
+            UnequipTools()
+            local First = true
+            repeat
+                for _, v in pairs(workspace.Ignored.Drop:GetChildren()) do
+                    if (v.Name == 'MoneyDrop') and (v:FindFirstChildOfClass('ClickDetector')) and LocalPlayer:DistanceFromCharacter(v.Position) < mag and (LastCashier and (v.Position - LastCashier.Position).Magnitude < mag) then
+                        
+                        LocalPlayer.CameraMinZoomDistance = 6
+                        LocalPlayer.CameraMaxZoomDistance = 6
+
+                        Camera.CameraSubject = v
+                        if LastCashier then
+                            LocalPlayer.Character:MoveTo(LastCashier.Position + Vector3.new(0, 3, 0))
                         end
+
+                        fireclickdetector(v:FindFirstChildOfClass('ClickDetector'))
+                        task.wait(0.1)
                     end
-                    if not GotCash then break end;
-                    task.wait(0.1)
-                until not checkcash()
-                Camera.CameraSubject = LocalPlayer.Character.Humanoid
-                Settings.Cash.Collecting = false
-            else
-                for _, cashier in ipairs(workspace.Cashiers:GetChildren()) do
-                    if cashier:FindFirstChildOfClass('Humanoid') and cashier.Humanoid.Health > 0 and not Settings.Cash.Collecting then
-                        if Alive() and not Settings.Cash.Collecting then
-                            local char = LocalPlayer.Character
-                            local hm = char.Humanoid
-                            repeat
-                                task.wait()
-                                if checkcash() then break end
-                                if cashier:FindFirstChildOfClass('Humanoid').Health <= 0 then
-                                    safespot()
-                                    break
-                                end
-                                if not char:FindFirstChild('Highlight') then
-                                    safespot()
-                                    EquipCombatTool()
-                                    if LocalPlayer.Character:FindFirstChildOfClass('Tool') then
-                                        LocalPlayer.Character:FindFirstChildOfClass('Tool'):Activate()
-                                    end
-                                else
-                                    local BrokenCashier = cashier.Head.CFrame == CFrame.new(-626.998474, 24.9000168, -285.062164, -1, 0, 0, 0, 1, 0, 0, 0, -1)
-                                    LocalPlayer.Character.HumanoidRootPart.CFrame = cashier.Open.CFrame * (BrokenCashier and CFrame.new(3, -1.5, 0) or CFrame.new(-2, -1.5, 0))
-                                    LastCashier = cashier.Open
-                                end
-                            until cashier:FindFirstChildOfClass('Humanoid').Health <= 0 or checkcash()
-                        end
+                end
+
+
+                if not checkcash() then break end;
+                task.wait(0.1)
+            until not checkcash()
+
+            for _, cashier in ipairs(workspace.Cashiers:GetChildren()) do
+                if cashier:FindFirstChildOfClass('Humanoid') and cashier.Humanoid.Health > 0 then
+                    if Alive() then
+            
+                        local char = LocalPlayer.Character
+                        local hm = char.Humanoid
+                        local be = char:FindFirstChild("BodyEffects")
+            
+                        repeat
+                            task.wait()
+                            if checkcash() then break end;
+                            Camera.CameraSubject = cashier.Open
+                            Camera.CFrame = CFrame.new(Camera.CFrame.Position) * CFrame.Angles(math.rad(-90), 0, 0);
+            
+                            if cashier:FindFirstChildOfClass('Humanoid').Health <= 0 then
+                                safespot();
+                                break
+                            end;
+            
+                            if not char:FindFirstChild('Highlight') then
+                                safespot();
+                                EquipCombatTool();
+                                if LocalPlayer.Character:FindFirstChildOfClass('Tool') and (be and be:FindFirstChild("Attacking") and not be.Attacking.Value) then
+                                    LocalPlayer.Character:FindFirstChildOfClass('Tool'):Activate();
+                                end;
+                            else
+
+                                local BrokenCashier = cashier.Head.CFrame == CFrame.new(-626.998474, 24.9000168, -285.062164, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+                                LocalPlayer.Character.HumanoidRootPart.CFrame = cashier.Open.CFrame * (BrokenCashier and CFrame.new(3, -1.5, 0) or CFrame.new(-2, -1.5, 0));
+                                LastCashier = cashier.Open;
+                            end;
+            
+                        until cashier:FindFirstChildOfClass('Humanoid').Health <= 0 or checkcash()
+                        
                     end
                 end
             end
+
         end
     end)
 end
 
 task.spawn(function()
     while task.wait(0.04) do
-        if Settings.Cash.Collecting then
-            Camera.CFrame = CFrame.new(Camera.CFrame.Position) * CFrame.Angles(math.rad(-180,180), math.rad(math.random(-180,180)), math.rad(math.random(-180,180)))
+        if Camera.CameraSubject.Name == "MoneyDrop" then
+            local cam2 = {90 , 180,-180,-90}
+
+            Camera.CFrame = CFrame.new(Camera.CFrame.Position) * CFrame.Angles(math.rad(cam2[math.random(1,4)]), math.rad(cam2[math.random(1,4)]), math.rad(cam2[math.random(1,4)]))
         end;
     end;
-end)
+end);
 
 Atms()
